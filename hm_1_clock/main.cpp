0a40b2da84e3c81f797eb416269f061666d9806a
Sankek <iziurov.as@phystech.edu>
2021-02-07 23:03:40 +0300

    task_3 updated task number and function comment

--- hm_1_clock/main.cpp
+++ hm_1_clock/main.cpp
@@ -2,13 +2,12 @@
 
 constexpr int temp{1}; // this const is redundant!
 
-void task_2_function(){
-  // nothing interesing here
-  // mmm.. commits...
+void task_3_function(){
+  // no comments.
 }
 
 int main() {
-  std::cout << "Now we are doing the second task!" << std::endl;
+  std::cout << "Now we are doing the third task!" << std::endl;
   std::cout << "Rebase is done!" << '\n';
   return 0;
 }
