6faf4ed21ace6613960e15cc38a315ac5190afdb
Sankek <iziurov.as@phystech.edu>
2021-02-07 21:53:13 +0300

    task_2 added function

--- hm_1_clock/main.cpp
+++ hm_1_clock/main.cpp
@@ -2,6 +2,10 @@
 
 constexpr int temp{1}; // this const is redundant!
 
+void task_2_function(){
+  // nothing interesing here
+}
+
 int main() {
   std::cout << "Now we are doing the second task!" << std::endl;
   return 0;
