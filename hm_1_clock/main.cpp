aa807f226bec2fac377f4b8c63b71326961728c4
Sankek <iziurov.as@phystech.edu>
2021-02-08 00:44:20 +0300

    task_4 Timer, code from seminar

--- hm_1_clock/main.cpp
+++ hm_1_clock/main.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
+#include <chrono>
+#include <vector>
+#include <algorithm>
 
-constexpr int temp{1}; // this const is redundant!
 
-void task_2_function(){
-  // maybe it's time to write something here?
-}
+class Timer{
+ public:
+  using Clock_t = std::chrono::steady_clock;
+  using time_point_t = Clock_t::time_point;
+  Timer() : m_begin(Clock_t::now()) {};
+  ~Timer(){
+    auto end = Clock_t::now();
+
+    std::cout << "milliseconds: " << std::chrono::duration_cast<
+        std::chrono::milliseconds> (end-m_begin).count() << '\n';
+  }
+ private:
+  time_point_t m_begin;
+
+};
 
 int main() {
-  std::cout << "Now we are doing the third task!" << std::endl;
-  std::cout << "Rebase is done!" << '\n';
+
+  std::vector v(100000U, 0);
+
+  for (auto i = 0U; i < std::size(v); ++i){
+    v[i] = 100000U-i;
+  }
+
+  {
+    Timer t;
+    std::sort(v.begin(), v.end());
+  }
   return 0;
 }
