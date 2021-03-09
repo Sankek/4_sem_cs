2d1543e7af1ec3b29228911f4385ed92c1e79fbf
Sankek <iziurov.as@phystech.edu>
2021-03-09 21:53:36 +0300

    Added txt files for input|output

--- hw_5/datetime_regex/datetime_regex.cpp
+++ hw_5/datetime_regex/datetime_regex.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 #include <string>
 #include <regex>
+#include <fstream>
+#include <sstream>
+
 
 int main(int argc, char **argv){
+  std::ifstream in("../input.txt");
+  std::ofstream out("../output.txt");
+  std::cout.rdbuf(out.rdbuf()); // redirect std::cout to output.txt
+
+  std::stringstream str_stream;
+  str_stream << in.rdbuf();
+  std::string user_str = str_stream.str();
 
   // date format: DD.MM.YYYY, time format: HH:MM:SS
-  std::cout << "Enter some text to find dates and times:\n";
-  while(!std::cin.fail()){
-    std::string user_str;
-    std::getline(std::cin, user_str);
-    std::regex pattern(R"(((0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[012])\.([0-9]{4}))|(([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])))");
+  std::cout << "Found dates and times:\n";
+  std::regex pattern(R"(((0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[012])\.([0-9]{4}))|(([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])))");
 
-    std::sregex_iterator begin(user_str.cbegin(), user_str.cend(), pattern);
-    std::sregex_iterator end;
+  std::sregex_iterator begin(user_str.cbegin(), user_str.cend(), pattern);
+  std::sregex_iterator end;
 
-    std::for_each(begin, end, [](const std::smatch & m)
-    {
-      if (m[1] != ""){
-        std::cout << "Found date: " << m[1] << '\n';
-      }
-      if (m[5] != ""){
-        std::cout << "Found time: " << m[5] << '\n';
-      }
-    });
+  std::for_each(begin, end, [](const std::smatch & m)
+  {
+    if (m[1] != ""){
+      std::cout << "Found date: " << m[1] << '\n';
+    }
+    if (m[5] != ""){
+      std::cout << "Found time: " << m[5] << '\n';
+    }
+  });
 
-  }
-
-  system("pause");
 
   return EXIT_SUCCESS;
 }
--- hw_5/email_regex/email_regex.cpp
+++ hw_5/email_regex/email_regex.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 #include <regex>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 
 int main(int argc, char **argv){
+  std::ifstream in("../input.txt");
+  std::ofstream out("../output.txt");
+  std::cout.rdbuf(out.rdbuf()); // redirect std::cout to output.txt
 
-//  const std::string user_str = "EMAIL: my_email228@phystech.edu, чтож@да.нет?, new_email = \"name.surname@mipt.ru\" ";
+  std::stringstream str_stream;
+  str_stream << in.rdbuf();
+  std::string user_str = str_stream.str();
 
-  std::cout << "Enter some text to find email domains:\n";
-  while(!std::cin.fail()){
-    std::string user_str;
-    std::getline(std::cin, user_str);
-    std::regex pattern(R"(([\-\w]+)@([\-\w]+\.[_\-[:alpha:]]+))");
+  std::cout << "Found email domains:\n";
+  std::regex pattern(R"(([\-\w]+)@(\w+[_\-\w]*\w+(\.[[:alpha:]]+)+))");
 
+  std::sregex_iterator begin(user_str.cbegin(), user_str.cend(), pattern);
+  std::sregex_iterator end;
 
-    std::sregex_iterator begin(user_str.cbegin(), user_str.cend(), pattern);
-    std::sregex_iterator end;
-
-    std::for_each(begin, end, [](const std::smatch & m)
-    {
-      std::cout << "Found e-mail: " << m[0] << ", domain: " << m[2] << '\n';
-    });
-  }
-
-  system("pause");
+  std::for_each(begin, end, [](const std::smatch & m)
+  {
+    std::cout << "Found e-mail: " << m[0] << ", domain: " << m[2] << '\n';
+  });
 
   return EXIT_SUCCESS;
 }
